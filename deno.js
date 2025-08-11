851ed33527ddbc90d1c2640166c55a811f91cffd
MiladDiMiTiR <ansarimilad1382@gmail.com>
2025-08-11 13:40:53 +0330

    Update deno.js

--- deno.js
+++ deno.js
@@ -1,9 +1,5 @@
 {
   "tasks": {
-    "start": "deno run -A bot.ts",
-    "dev": "deno run --watch -A bot.ts"
-  },
-  "compilerOptions": {
-    "lib": ["deno.window"]
+    "start": "deno run -A bot.ts"
   }
 }
